58ea0bf39d53b601e0308b462ec5c44248a6fb22
rutherford36 <kiransi_me@hotmail.com>
2021-04-08 18:40:50 +0530

    Create exercise_2.2.py

---
+++ exercise_2.2.py
@@ -0,0 +1,7 @@
+num = int(input("Enter Number: "))
+div_num = int(input("Enter Divide Number: "))
+output = (num // div_num)
+if output == 0:
+    print("This is a Even number")
+else:
+    print("This is not a Even number")
