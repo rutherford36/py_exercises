3eec50b9e72cb6104d258af2270feac8338a6b99
rutherford36 <kiransi_me@hotmail.com>
2021-04-08 14:45:30 +0530

    Create exercise_2.py

---
+++ exercise_2.py
@@ -0,0 +1,7 @@
+# Program to find odd or even
+number = int(input("Enter a Number:"))
+result = (number % 2)
+if result == 0:
+    print("Given Number is Even")
+else:
+    print("Given Number is Odd")
